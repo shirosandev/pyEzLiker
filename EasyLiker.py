5046fa86aa2480ef8929228a215b1014b03eb25d
Vitalii Stolyarov <vtlstolyarov@gmail.com>
2022-06-18 10:20:27 -0400

    Normalize spacings and indentations in accordance with PEP 8

--- EasyLiker.py
+++ EasyLiker.py
@@ -7,46 +7,47 @@ class SendRequestError(Exception):
     pass
 
 
+class EasyLiker:
 
-class EasyLiker(object):
-
-
-    def __init__(self, token) -> EasyLiker:
-        self.token = token
-        self.headers = {
-            "Content-type": "application/json"
+    def __init__(self, token: str) -> EasyLiker:
+        self._token = token
+        self._headers = {
+            "Content-type": "application/json",
             }
-        self.api_url="https://easyliker.ru/api"
+        self._api_url = "https://easyliker.ru/api"
 
-        self.default_data = {
-            "api_token": self.token, 
-            "method": "None", 
-            "version": 2.0}
+        self._default_data = {
+            "api_token": self._token,
+            "method": "None",
+            "version": 2.0,
+            }
 
     def __req_worker(self, data):
-        response = loads(post(self.api_url, data=dumps(data), headers=self.headers).text)
+        response = loads(post(self._api_url, data = dumps(data), headers = self._headers).text)
         if "error" in response.keys():
             raise SendRequestError(f"Ошибка при отправке запроса: {response['error']}")
         return response['response']
 
     def get_account_balance(self) -> float:
-        temp_data = self.default_data
-        temp_data['method']  = "getBalance"
+        temp_data = self._default_data
+        temp_data['method'] = "getBalance"
         return self.__req_worker(temp_data)
 
     def get_services(self) -> dict:
-        temp_data = self.default_data
-        temp_data['method']  = "getServices"
+        temp_data = self._default_data
+        temp_data['method'] = "getServices"
         return self.__req_worker(temp_data)
 
-    def create_task(self, 
-            website: str, type: str,
-            quality: str, link: str,
-            count: str, option: list | int = None
-            ) -> dict:
-
-        temp_data = self.default_data
-        temp_data['method']  = "createTask"
+    def create_task(
+            self,
+            website: str,
+            type: str,
+            quality: str,
+            link: str,
+            count: str,
+            option: list | int = None) -> dict:
+        temp_data = self._default_data
+        temp_data['method'] = "createTask"
         temp_data['website'] = website
         temp_data['quality'] = quality
         temp_data['type'] = type
@@ -54,21 +55,19 @@ class EasyLiker(object):
         temp_data['count'] = count
         if option:
             temp_data['option'] = option
-
         return self.__req_worker(temp_data)
 
-    def get_tasks(self,
-            id : int =None, count : int=None, 
-            offset : int=None,) -> dict:
-        
-        temp_data = self.default_data
-
+    def get_tasks(
+            self,
+            id: int = None,
+            count: int = None,
+            offset: int = None) -> dict:
+        temp_data = self._default_data
         if id:
             temp_data['id'] = id
         if count:
             temp_data['count'] = count
         if offset:
             temp_data['offset'] = offset
-        temp_data['method']  = "getTasks"
-        return self.__req_worker(temp_data) 
-
+        temp_data['method'] = "getTasks"
+        return self.__req_worker(temp_data)
